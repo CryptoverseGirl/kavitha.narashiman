1905ab6ac8b49706d9d3efd6187db1c100cd2f99
vivekgorrepati <54054294+vivekgorrepati@users.noreply.github.com>
2022-04-27 22:25:10 +0530

    desktop edition push

--- GalleryShowroom/Assets/Photon/PhotonVoice/Demos/DemoVoicePun/Scripts/FirstPersonController.cs
+++ GalleryShowroom/Assets/Photon/PhotonVoice/Demos/DemoVoicePun/Scripts/FirstPersonController.cs
@@ -8,11 +8,13 @@
 // <author>developer@photonengine.com</author>
 // ----------------------------------------------------------------------------
 
-namespace ExitGames.Demos.DemoPunVoice {
+namespace ExitGames.Demos.DemoPunVoice
+{
 
     using UnityEngine;
 
-    public class FirstPersonController : BaseController {
+    public class FirstPersonController : BaseController
+    {
 
         [SerializeField]
         private MouseLookHelper mouseLook = new MouseLookHelper();
@@ -20,16 +22,19 @@ namespace ExitGames.Demos.DemoPunVoice {
         private float oldYRotation;
         private Quaternion velRotation;
 
-        public Vector3 Velocity {
+        public Vector3 Velocity
+        {
             get { return this.rigidBody.velocity; }
         }
 
-        protected override void SetCamera() {
+        protected override void SetCamera()
+        {
             base.SetCamera();
             this.mouseLook.Init(this.transform, this.camTrans);
         }
 
-        protected override void Move(float h, float v) {
+        protected override void Move(float h, float v)
+        {
             // always move along the camera forward as it is the direction that it being aimed at
             Vector3 desiredMove = this.camTrans.forward * v + this.camTrans.right * h;
             desiredMove.x = desiredMove.x * this.speed;
@@ -38,11 +43,37 @@ namespace ExitGames.Demos.DemoPunVoice {
             this.rigidBody.velocity = desiredMove;
         }
 
-        private void Update() {
-            this.RotateView();
+        protected override void UpdateAnimator(float h, float v)
+        {
+            Debug.Log("in updat enaimator");
+
+            Vector3 Movement = new Vector3(h, 0, v);
+
+            float velocityZ = Vector3.Dot(Movement.normalized, transform.forward);
+            float velocityX = Vector3.Dot(Movement.normalized, transform.right);
+
+            animator.SetFloat("VelocityX", velocityX, 0.1f, Time.deltaTime);
+            animator.SetFloat("VelocityZ", velocityZ, 0.1f, Time.deltaTime);
+            //// always move along the camera forward as it is the direction that it being aimed at
+            //Vector3 desiredMove = this.camTrans.forward * v + this.camTrans.right * h;
+            //desiredMove.x = desiredMove.x * this.speed;
+            //desiredMove.z = desiredMove.z * this.speed;
+            //desiredMove.y = 0;
+            //this.rigidBody.velocity = desiredMove;
         }
 
-        private void RotateView() {
+        private void Update()
+        {
+            if (Input.GetMouseButton(1))
+            {
+                Debug.Log("right click down");
+                this.RotateView();
+            }
+
+        }
+
+        private void RotateView()
+        {
             // get the rotation before it's changed
             this.oldYRotation = this.transform.eulerAngles.y;
             this.mouseLook.LookRotation(this.transform, this.camTrans);
